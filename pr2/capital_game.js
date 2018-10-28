b218e69748a19a35991d1e7737ac5d8a0a4979d2
tanzeelak <tnkhan8042@gmail.com>
2018-10-28 14:58:11 -0700

    random first question

--- pr2/capital_game.js
+++ pr2/capital_game.js
@@ -2,5 +2,22 @@
 // has finished loading in the browser.
 
 $( document ).ready(function() {
-  var country_capital_pairs = pairs
+  var country_capital_pairs = pairs;
+  countryCapitalPair = updatePair();
+
+  $('#pr2__submit').click(function() {
+    countryCapitalPair = updatePair();
+  });
 });
+
+function updatePair() {
+  var question = $('#pr2__question');
+  var answer = $('#pr2__answer');
+
+  var randNum = Math.floor(Math.random() * 170);
+  pair = pairs[randNum];
+  question.html(pair['country']);
+  answer.focus();
+  answer.val('');
+  return pair;
+}
