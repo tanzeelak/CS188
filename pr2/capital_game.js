53a1c1735aa4d91b99f4279c7c32d8f77062c5e6
tanzeelak <tnkhan8042@gmail.com>
2018-10-28 15:25:49 -0700

    checking if correct capital

--- pr2/capital_game.js
+++ pr2/capital_game.js
@@ -1,23 +1,36 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
+var currentPair; 
 
 $( document ).ready(function() {
   var country_capital_pairs = pairs;
-  countryCapitalPair = updatePair();
+  updatePair();
 
   $('#pr2__submit').click(function() {
-    countryCapitalPair = updatePair();
+    evaluateAnswer();
+    updatePair();
   });
 });
+
+function evaluateAnswer() {
+  var answer = $('#pr2__answer').val();
+  console.log('answer: ' + answer);
+  console.log('capital: ' + currentPair['capital']);
+  if (currentPair['capital'] == answer) {
+    console.log('correcteroni');
+  }
+  else {
+    console.log('wrongo');
+  }
+}
 
 function updatePair() {
   var question = $('#pr2__question');
   var answer = $('#pr2__answer');
 
   var randNum = Math.floor(Math.random() * 170);
-  pair = pairs[randNum];
-  question.html(pair['country']);
+  currentPair = pairs[randNum];
+  question.html(currentPair['country']);
   answer.focus();
   answer.val('');
-  return pair;
 }
